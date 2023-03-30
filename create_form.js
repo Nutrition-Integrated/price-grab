873c659f76ecee212e83ac2af8c5e7254e90d3e9
danwatt1987 <44509557+danwatt1987@users.noreply.github.com>
2023-03-30 10:35:19 +0100

    w h

--- create_form.js
+++ create_form.js
@@ -10,7 +10,7 @@ let author = "me";
 let message = "Message...";
 let time = "15:21";
 
-var html = `<div style="z-index:99;position:fixed;background-color:white;height:100;width:100">
+var html = `<div style="z-index:99;position:fixed;background-color:white;height:100px;width:100px">
                 <li><span class="author">${author}</span>
                 <span class="message">${message}</span>
                 <span class="time"><div class="line"></div>${time}</span></li>
