9e07465ca097e8cbf0f6fd876a77422a3fbd635f
danwatt1987 <44509557+danwatt1987@users.noreply.github.com>
2023-03-30 10:24:40 +0100

    first commit

---
+++ create_form.js
@@ -0,0 +1,18 @@
+function appendHtml(el, str) {
+  var div = document.createElement('div'); //container to append to
+  div.innerHTML = str;
+  while (div.children.length > 0) {
+    el.appendChild(div.children[0]);
+  }
+}
+
+let author = "me";
+let message = "Message...";
+let time = "15:21";
+
+var html = `<div style="z-index:99;position:fixed;background-color:white;height:100;width:100">
+                <li><span class="author">${author}</span>
+                <span class="message">${message}</span>
+                <span class="time"><div class="line"></div>${time}</span></li>
+                </div>`;
+appendHtml(document.body, html);
